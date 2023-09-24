bf9819f488556a09aa6cdd654842dfd3802d490c
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-09-24 18:18:33 +0600

    Added applied jobs section and made it functional.

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,21 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredApplication } from "../../utility/localStorage";
+import Job from "../FeaturedJobs/Job/Job";
 
 const AppliedJobs = () => {
   const jobs = useLoaderData();
+
+  const [appliedjobs, setAppliedJobs] = useState([]);
+
+  const [jobsApplied, setJobsApplied] = useState(false);
 
   useEffect(() => {
     const storedJobIds = getStoredApplication();
     if (storedJobIds.length > 0) {
       const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
-      console.log(jobsApplied);
+      setAppliedJobs(jobsApplied);
+      setJobsApplied(true);
     } else {
-      console.log("NONE");
+      setJobsApplied(false);
     }
   }, []);
 
-  return <div>hshaha</div>;
+  return (
+    <>
+      <h1 className="text-center font-main font-extrabold text-5xl mt-12 mb-12">
+        Applied Jobs
+      </h1>
+
+      {jobsApplied && (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8 mb-52">
+          {appliedjobs.map((job) => (
+            <Job key={job.id} job={job}></Job>
+          ))}
+        </div>
+      )}
+
+      {jobsApplied || (
+        <div className="text-center w-96 mx-auto text-2xl font-bold p-12 rounded-lg h-[550px]">
+          Apply to jobs to see them here
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AppliedJobs;
